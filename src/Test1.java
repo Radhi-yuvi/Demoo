2407167236e8c6f7a446a4360262da6c404cf047
DELL <DELL@DESKTOP-9IM227E>
2023-01-04 17:58:08 +0530

    first commit..

--- src/Test1.java
+++ src/Test1.java
@@ -2,8 +2,24 @@
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try
+        {
+              System.out.printf("1");
+            int sum = 9 / 0;
+              System.out.printf("2");
+        }
+        catch(ArithmeticException e)
+        {
+              System.out.printf("3");
+        }
+         catch(Exception e)
+        {
+              System.out.printf("4");
+        }
+        finally
+        {
+           System.out.printf("5");
+        }
 	}
 
 }
